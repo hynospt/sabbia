c181a6cd48fc1f54a6c75740f27f70c5912faa4e
Haryono Sariputra <Haryono Sariputra>
2016-08-11 01:12:16 +0700

    chain edit add

--- app/Http/Controllers/ChainController.php
+++ app/Http/Controllers/ChainController.php
@@ -8,6 +8,8 @@ use App\Http\Requests;
 
 use App\Chain;
 
+use Validator;
+
 class ChainController extends Controller
 {
     public function edit(){
@@ -17,7 +19,59 @@ class ChainController extends Controller
 
     public function update(Request $request){
     	$chain = Chain::find(1);
-    	$chain->update($request->all());
+    	$path = public_path('saved_images/chain');
+    	$image1 = $request->file('image1');
+        $image2 = $request->file('image2');
+        $image1_old = $request->input('image1_old');
+        $image2_old = $request->input('image2_old');
+
+        $rules = [
+            'title1'         => 'required',
+            'content1'     => 'required',
+            'title2'     => 'required',
+            'content2'     => 'required',
+            'image1'        => 'required|mimes:png,jpg,jpeg',
+            'image2'        => 'required|mimes:png,jpg,jpeg'
+        ];
+
+        $validator = Validator::make($request->all() , $rules);
+        if($validator->fails()){
+            flash('Oops, Something Went Wrong!', 'danger');
+            return redirect()->back()->withInput()->withErrors($validator->messages());
+        }
+
+        if($image1){
+            $image1FileName = '1_img'.date('YmdHis').$image1->getClientOriginalName();
+            $image1->move($path , $image1FileName);
+
+            if($image1_old){
+                $deletePath1 = $path.'/'.$image1_old;
+                if(file_exists($deletePath1)){
+                    unlink($deletePath1);
+                }
+            }
+
+            $chain->image1 = $image1FileName;
+
+        }
+
+        if($image2){
+            $image2FileName = '2_img'.date('YmdHis').$image2->getClientOriginalName();
+            $image2->move($path, $image2FileName);
+            if($image2_old){
+                $deletePath2 = $path.'/'.$image2_old;
+                if(file_exists($deletePath2)){
+                    unlink($deletePath2);
+                }   
+            }
+            $chain->image2 = $image2FileName;
+        }
+
+        $chain->title1 = $request->input('title1');
+        $chain->title2 = $request->input('title2');
+        $chain->content1 = $request->input('content1');
+        $chain->content2 = $request->input('content2');
+        $chain->save();
 
     	return redirect()->back();
     }
--- resources/views/admin/pages/about/chain/edit.blade.php
+++ resources/views/admin/pages/about/chain/edit.blade.php
@@ -6,7 +6,7 @@ Edit Chain
 
 @section('body')
   <div class="content-wrapper">
-    
+    @include('errors.formErrors')
     <section class="content-header">
       <h1>
         Edit Chain
@@ -18,20 +18,74 @@ Edit Chain
       </ol>
 
       <section class="content">
-         <form name="editChainFrom" action="{{action('ChainController@update')}}" method="post">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input name="mainTag" type="hidden" value="{{$chain->id}}" / ><br/>
-        title1 <input name="title1" type="" value="{{$chain->title1}}" / ><br/>
-        content1 <input name="content1" type="" value="{{$chain->content1}}" / ><br/>
-        image1 <input name="image1" type="" value="{{$chain->image1}}" / ><br/>
-        title2 <input name="title2" type="" value="{{$chain->title2}}" / ><br/>
-        content2 <input name="content2" type="" value="{{$chain->content2}}" / ><br/>
-        image2 <input name="image2" type="" value="{{$chain->image2}}" / ><br/>
-        <br/>
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="box box-primary">
+              <div class="box-header with-border">
+                <h3 class="box-title">Edit Chain</h3>
+              </div>
+              <form name="editChainFrom" action="{{action('ChainController@update')}}" method="post" enctype="multipart/form-data">
+                <div class="box-body">
+                  <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                  <input type="hidden" name="id" value="{{$chain->id}}">
+                  
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Page Title I</label>
+                      {{ Form::text('title1', $chain->title1 , ['class' => 'form-control' , 'placeholder' => 'page title']) }}
+                    </div>
+                  </div>
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Content I</label>
+                      {{ Form::text('content1', $chain->content1 , ['class' => 'form-control' , 'placeholder' => 'main title']) }}
+                    </div>
+                  </div>
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Page Title II</label>
+                      {{ Form::text('title2', $chain->title2 , ['class' => 'form-control' , 'placeholder' => 'page title']) }}
+                    </div>
+                  </div>
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Content II</label>
+                      {{ Form::text('content2', $chain->content2 , ['class' => 'form-control' , 'placeholder' => 'sub title']) }}
+                    </div>
+                  </div>
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Image 1</label>
+                      <input type="file" name="image1">
+                      <input type="hidden" name="image1_old" value="{{$chain->image1}}">
+                    </div>
+                  </div>
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Image 2</label>
+                      <input type="file" name="image2">
+                      <input type="hidden" name="image2_old" value="{{$chain->image2}}">
+                    </div>
+                  </div>
+
+                </div>
+                <div class="box-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+
+            </div>
+          </div>
+        </div>
+         
 
 
-        <input type="submit">
-        </form>
       </section>
     </section>
 
